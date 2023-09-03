dff91df4e82d86cf899d0bbb06cec50d2976ceca
Carole Arbogast <arbogast.carole@gmail.com>
2023-09-03 10:27:39 +0300

    feat: use date-fns to display article publish date

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { ArticleInfo, request } from "@/app/page";
+import { format } from "date-fns";
 
 interface Article {
   id: string;
   creationDate: Date;
-  publishDate: Date;
+  publishDate: string;
   title: string;
   content: string;
 }
@@ -17,10 +18,12 @@ const getData = async (id: string) => {
 
 export async function NewsArticle({ params }: { params: { id: string } }) {
   const data = await getData(params.id);
+  const publishDate = new Date(data.publishDate);
+  const formattedDate = `${format(publishDate, "dd.MM.yyyy H.m")}`;
 
   return (
     <div>
-      {data.publishDate}
+      {formattedDate}
       <h1>{data.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: data.content }}></div>
     </div>
